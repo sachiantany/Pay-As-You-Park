f700155250a3446e4848f835783e2637650a57d8
yoshadev <yoshan@oursrilanka.com>
2021-04-20 17:34:09 +0530

    initial load

---
+++ payp-web/src/components/about-app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class AboutApp extends Component {
+
+  render() {
+    return (
+      <div>
+          <h1>About App</h1>
+      </div>
+    );
+  }
+}
