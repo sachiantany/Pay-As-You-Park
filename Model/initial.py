925f606c2fbb758f225c2f5c5c21af8bb4631fbb
it18013092 <it18013092@my.sliit.lk>
2021-06-26 00:34:45 +0530

    parking history ARIMA

--- Model/initial.py
+++ Model/initial.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import pandas as pd
 pd.set_option('display.max_columns', 500)
 
@@ -14,9 +8,23 @@ get_ipython().run_line_magic('matplotlib', 'inline')
 
 import matplotlib.ticker as ticker
 
+from statsmodels.tsa.stattools import adfuller, acf, pacf
 
-# In[2]:
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+from statsmodels.tsa.arima_model import ARIMA
+from statsmodels.tsa.seasonal import seasonal_decompose
+from pandas.plotting import autocorrelation_plot
+
+from sklearn import metrics
+
+from sklearn.linear_model import LinearRegression
+
+import warnings
+
+import datetime
+from datetime import timedelta 
 
 def sample_plots_by_scn(df, num_graphs, num_per_row, fig_width=16, hspace=0.6):
     """Print a sample of the data by Parking location, identified with the field SystemCodeNumber
@@ -45,9 +53,61 @@ def sample_plots_by_scn(df, num_graphs, num_per_row, fig_width=16, hspace=0.6):
         plt.sca(ax)
         plt.xticks(rotation=45)
 
+def test_stationarity(timeseries, window):
 
-# In[ ]:
+   #Determing rolling statistics
+    rolmean = timeseries.rolling(window=window).mean()
+    rolstd = timeseries.rolling(window=window).std()
 
+   #Plot rolling statistics:
+    fig = plt.figure(figsize=(12, 8))
+    orig = plt.plot(timeseries.iloc[window:], color='blue',label='Original')
+    mean = plt.plot(rolmean, color='red', label='Rolling Mean')
+    std = plt.plot(rolstd, color='black', label = 'Rolling Std')
+    plt.legend(loc='best')
+    plt.title('Rolling Mean & Standard Deviation')
+    plt.show()
 
+    #Perform Dickey-Fuller test:
+    print ('Results of Dickey-Fuller Test:')
+    dftest = adfuller(timeseries, autolag='AIC')
+    dfoutput = pd.Series(dftest[0:4], index=['Test Statistic','p-value','#Lags Used','Number of Observations Used'])
+    for key,value in dftest[4].items():
+        dfoutput['Critical Value (%s)'%key] = value
+    print (dfoutput)
+        
+        
+def subplots_acf_pacf(series):
+    fig = plt.figure(figsize=(12,8))
+    ax1 = fig.add_subplot(211)
+    fig = plot_acf(series, lags=40, ax=ax1)
+    ax2 = fig.add_subplot(212)
+    fig = plot_pacf(series, lags=40, ax=ax2)
+    plt.show()
 
+def test_stationarity(timeseries, window):
 
+   #Determing rolling statistics
+    rolmean = timeseries.rolling(window=window).mean()
+    rolstd = timeseries.rolling(window=window).std()
+
+   #Plot rolling statistics:
+    fig = plt.figure(figsize=(12, 8))
+    orig = plt.plot(timeseries.iloc[window:], color='blue',label='Original')
+    mean = plt.plot(rolmean, color='red', label='Rolling Mean')
+    std = plt.plot(rolstd, color='black', label = 'Rolling Std')
+    plt.legend(loc='best')
+    plt.title('Rolling Mean & Standard Deviation')
+    plt.show()
+
+    #Perform Dickey-Fuller test:
+    print ('Results of Dickey-Fuller Test:')
+    dftest = adfuller(timeseries, autolag='AIC')
+    dfoutput = pd.Series(dftest[0:4], index=['Test Statistic','p-value','#Lags Used','Number of Observations Used'])
+    for key,value in dftest[4].items():
+        dfoutput['Critical Value (%s)'%key] = value
+    print (dfoutput)
+
+def report_metrics(y_true, y_pred):
+    print("Explained Variance:\n\t", metrics.explained_variance_score(y_true, y_pred))
+    print("MAE:\n\t", metrics.mean_absolute_error(y_true, y_pred))
