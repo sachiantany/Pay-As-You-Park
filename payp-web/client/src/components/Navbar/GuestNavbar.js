7fda97138c870011f425ab54c87c3ce746491b9d
sachiantany <sachiantany>
2021-09-19 09:24:54 +0530

    header fix

--- payp-web/client/src/components/Navbar/GuestNavbar.js
+++ payp-web/client/src/components/Navbar/GuestNavbar.js
@@ -8,6 +8,7 @@ import decode from 'jwt-decode';
 import * as actionType from '../../constants/actionTypes';
 import useStyles from './styles';
 import './Header.css';
+import Icon from "../../images/icon.svg"
 
 
 const GuestNavbar = () => {
@@ -38,77 +39,81 @@ const GuestNavbar = () => {
     }, [location]);
 
     return ( 
-        <header>
-        <div className="container2">
-            <br/>
-            <nav>
-                <ul className="nav-list nav-list-mobile">
-                    <li className="nav-item">
-                        <div className="mobile-menu">
-                            <span className="line line-top"></span>
-                            <span className="line line-bottom"></span>
-                        </div>
-                    </li>
-                    <li className="nav-item">
-                        <a href="index.html" className="nav-link nav-link-apple"></a>
-                    </li>
-                    <li className="nav-item">
-                        <a href="#" className="nav-link nav-link-bag"></a>
-                    </li>
-                   
-                </ul>
-                {/* <!-- /.nav-list nav-list-mobile -->  */}
-                <ul className="nav-list nav-list-larger">
-                    <li className="nav-item search-hiden">
-                      
-                        <input className="nav-link nav-link-searchbar" type="text" 
-                            placeholder="&#xF002; Search apple.com" 
-                            style={{fontFamily:"Arial, FontAwesome"}} />
-                      
-                    </li>
-                    <li className="nav-item nav-item-hidden">
-                        <a href="/" className="nav-link nav-link-apple"></a>
-                    </li>
-                    <li className="nav-item">
-                        <a href="mac" className="nav-link">Pay as you park</a>
-                    </li>
-                
-                    <li className="nav-item">
-                        <a href="#" className="nav-link">Availability</a>
-                    </li>
-                    <li className="nav-item">
-                        <a href="#" className="nav-link">About</a>
-                    </li>
-                    <li className="nav-item">
-                        <a href="#" className="nav-link">Support</a>
-                    </li>
-                    <li className="nav-item">
-                        <a href="#" className="nav-link nav-link-search"></a>
-                    </li>
-                    <li className="nav-item">
-                            {/* <a href="#" className="nav-link nav-link-bag"></a> */}
-                        {user?.result ? (
-                            
-                              <div className={classes.profile}>
-                                <Avatar className={classes.purple} alt={user?.result.name} src={user?.result.imageUrl}>{user?.result.name.charAt(0)}</Avatar>
-                                <Typography className={classes.userName} variant="h6">{user?.result.name}</Typography>
-                            
-                                <Button  variant="contained" className={classes.logout} color="primary" onClick={logout}>Logout</Button>
+        <AppBar className={classes.appBar} position="static" color="inherit">
+            <header>
+            <div className="container2">
+                <br/>
+                <nav>
+                    <ul className="nav-list nav-list-mobile">
+                        <li className="nav-item">
+                            <div className="mobile-menu">
+                                <span className="line line-top"></span>
+                                <span className="line line-bottom"></span>
                             </div>
-                           
-                            
-                        ) : (
-                             <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
-                         )}
-                    </li>
-                  
-                  
-                </ul> 
+                        </li>
+                        <li className="nav-item">
+                            <a href="index.html" className="nav-link nav-link-apple"></a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="#" className="nav-link nav-link-bag"></a>
+                        </li>
                     
-            </nav>
+                    </ul>
+                    {/* <!-- /.nav-list nav-list-mobile -->  */}
+                    <ul className="nav-list nav-list-larger">
+                        <li className="nav-item search-hiden">
+                        
+                            <input className="nav-link nav-link-searchbar" type="text" 
+                                placeholder="&#xF002; Search apple.com" 
+                                style={{fontFamily:"Arial, FontAwesome"}} />
+                        
+                        </li>
+                        <li className="nav-item nav-item-hidden">
+                            {/* <a href="/" className="nav-link nav-link-apple"></a> */}
 
-        </div>
-    </header>
+                            <img src={Icon} className="nav-link-apple"></img>
+                        </li>
+                        <li className="nav-item">
+                            <a href="mac" className="nav-link">Pay as you park</a>
+                        </li>
+                    
+                        <li className="nav-item">
+                            <a href="#" className="nav-link">Availability</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="#" className="nav-link">About</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="#" className="nav-link">Support</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="#" className="nav-link nav-link-search"></a>
+                        </li>
+                        <li className="nav-item">
+                                {/* <a href="#" className="nav-link nav-link-bag"></a> */}
+                            {user?.result ? (
+                                
+                                <div className={classes.profile}>
+                                    <Avatar className={classes.purple} alt={user?.result.name} src={user?.result.imageUrl}>{user?.result.name.charAt(0)}</Avatar>
+                                    <Typography className={classes.userName} variant="h6">{user?.result.name}</Typography>
+                                
+                                    <Button  variant="contained" className={classes.logout} color="primary" onClick={logout}>Logout</Button>
+                                </div>
+                            
+                                
+                            ) : (
+                                <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
+                            )}
+                        </li>
+                    
+                    
+                    </ul> 
+                        
+                </nav>
+
+            </div>
+        </header>
+    </AppBar>
     )
     }
 
--- payp-web/client/src/components/Navbar/styles.js
+++ payp-web/client/src/components/Navbar/styles.js
@@ -3,13 +3,13 @@ import { deepPurple } from '@material-ui/core/colors';
 
 export default makeStyles((theme) => ({
   appBar: {
-    borderRadius: 15,
-    margin: '30px 0',
+    // borderRadius: 15,
+    marginBottom: '30px',
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    padding: '10px 50px',
+    // padding: '10px 50px',
   },
   heading: {
     color: 'rgba(0,183,255, 1)',
